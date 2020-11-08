a990316b8d61237e53ff031c1e7a639afdb550f1
glebboos <boosgleb@gmail.com>
2020-11-08 18:17:45 +0300

    Добавлена 11 задачка

--- src/Task 2-11.kt
+++ src/Task 2-11.kt
@@ -1,9 +1,7 @@
 import java.util.*
 
 fun main(args: Array<String>) {
-    var sotr: Map<String, Int> = mapOf()
-    //print("$sotr ")
-
+    val sotr = mutableMapOf<String, Int>()
     do {
         println("Меню:")
         println("1. Добавить сотрудника в базу")
@@ -14,11 +12,25 @@ fun main(args: Array<String>) {
         when(a.toInt()){
             1 ->
             {
-
+                print("Введите ФИО сотрудника: ")
+                val userText1: String? = readLine()
+                var x1: String = userText1.toString()
+                print("Введите рабочее место сотрудника ")
+                val userText2: String? = readLine()
+                var x2: String = userText2.toString()
+                var x22=x2.toInt()
+                var sotr2: Map<String, Int> = sortedMapOf(x1 to x22)
+                sotr.putAll(sotr2)
+                println("$sotr")
             }
             2 ->
             {
-
+                print("Введите рабочее место сотрудника для поиска: ")
+                val userText1: String? = readLine()
+                var x1: String = userText1.toString()
+                var x11=x1.toInt()
+                val keys = sotr.filterValues { it == x11 }.keys
+                println("$keys")
             }
             0 -> println("Выход из программы! ")
             else -> println("Ошибка! Некорректное значение!")
