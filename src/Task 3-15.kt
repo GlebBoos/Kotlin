462f94126d09ed85fc49b04fc6ba6a499132e2c9
glebboos <boosgleb@gmail.com>
2020-11-18 20:56:44 +0300

    Добавлена 15 задачка

---
+++ src/Task 3-15.kt
@@ -0,0 +1,22 @@
+fun main(args: Array<String>) {
+
+
+    print("Введите кол-во строк матрицы: ")
+    val userText1: String? = readLine()
+    val n: String = userText1.toString()
+    val n1=n.toInt();
+    println();
+
+    var lis : ArrayList<String> = arrayListOf()
+
+    for (i in 0 until n1){
+            print("Введите $i значение массива: ")
+            var userText: String? = readLine()
+            var x: String = userText.toString()
+            lis.add(x)
+        }
+
+    val vyv = {println(lis)}
+    vyv()
+}
+
